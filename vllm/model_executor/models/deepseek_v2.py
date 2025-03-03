Fix MoE Router Precision to FP32

vllm/model_executor/models/deepseek_v2.py
@@ -137,6 +137,7 @@ class DeepseekV2MoE(nn.Module):
         self.gate = ReplicatedLinear(config.hidden_size,
                                      config.n_routed_experts,
                                      bias=False,
+                                     params_dtype=torch.float32,
                                      quant_config=None,
                                      prefix=f"{prefix}.gate")
         if config.topk_method == "noaux_tc":
@@ -200,7 +201,7 @@ class DeepseekV2MoE(nn.Module):
                     ANALYSIS_CACHE_DYNAMIC[-1][name][self.layer_idx]["shared_experts_outputs"] = torch.norm(shared_output, p=p, dim=-1, dtype=torch.float32).cpu()
 
         # router_logits: (num_tokens, n_experts)
-        router_logits, _ = self.gate(hidden_states)
+        router_logits, _ = self.gate(hidden_states.type(torch.float32))
         final_hidden_states = self.experts(
             hidden_states=hidden_states,
             router_logits=router_logits